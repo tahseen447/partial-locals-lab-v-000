6f61bee6c75083d507bc14db110457b36ea1d98a
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2019-01-26 19:03:49 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -14,6 +14,11 @@ class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
 
+
+  validates :name, presence: true
+  validates :hometown, presence: true
+  validates :birthday, presence: true
+
   def self.search(query)
     if query.present?
       where('name LIKE?', "%#{query]}%")
